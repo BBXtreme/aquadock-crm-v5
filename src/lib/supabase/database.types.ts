42414a6be76b3becdb2375138bdd71f70220f335
BBXtreme <marco.a.graf@icloud.com>
2026-03-21 21:18:52 +0100

    style: change quotes from single to double in type exports

--- src/lib/supabase/database.types.ts
+++ src/lib/supabase/database.types.ts
@@ -270,9 +270,9 @@ export interface Database {
 }
 
 // Export table types
-export type Company = Database['public']['Tables']['companies']['Row'];
-export type Contact = Database['public']['Tables']['contacts']['Row'];
-export type Reminder = Database['public']['Tables']['reminders']['Row'];
-export type TimelineEntry = Database['public']['Tables']['timeline']['Row'];
-export type EmailLog = Database['public']['Tables']['email_log']['Row'];
-export type EmailTemplate = Database['public']['Tables']['email_templates']['Row'];
+export type Company = Database["public"]["Tables"]["companies"]["Row"];
+export type Contact = Database["public"]["Tables"]["contacts"]["Row"];
+export type Reminder = Database["public"]["Tables"]["reminders"]["Row"];
+export type TimelineEntry = Database["public"]["Tables"]["timeline"]["Row"];
+export type EmailLog = Database["public"]["Tables"]["email_log"]["Row"];
+export type EmailTemplate = Database["public"]["Tables"]["email_templates"]["Row"];
