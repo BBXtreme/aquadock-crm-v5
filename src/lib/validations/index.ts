7985ab01a32d745fe34e486c16712f42cc1920eb
BBXtreme <marco.a.graf@icloud.com>
2026-04-03 17:33:42 +0200

    refactor: update imports and export order in validations and client component

--- src/lib/validations/index.ts
+++ src/lib/validations/index.ts
@@ -1,6 +1,6 @@
 // Zod schemas – single source of truth after DTO migration (April 2026)
 export * from "./company";
 export * from "./contact";
+export * from "./email-template";
 export * from "./reminder";
 export * from "./timeline";
-export * from "./email-template";
