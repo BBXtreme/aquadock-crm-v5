06cb14f965a2658dd9234cf9019712aa7320a298
BBXtreme <marco.a.graf@icloud.com>
2026-04-04 17:02:47 +0200

    refactor: change z import to type import in ReminderEditForm

--- src/components/features/reminder/ReminderEditForm.tsx
+++ src/components/features/reminder/ReminderEditForm.tsx
@@ -8,7 +8,7 @@ import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
-import { z } from "zod";
+import type { z } from "zod";
 
 import { Button } from "@/components/ui/button";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
